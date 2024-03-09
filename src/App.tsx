77e2d1ef4e491c9eaef3511439ed554ebab8f96d
Nicholas Thomas <nicholas18thomas@gmail.com>
2024-03-08 22:02:02 -0700

    create app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
 import React from 'react';
 import logo from './logo.svg';
+import CollegeBasketballTeams from './CollegeBasketballTeams.json';
 import './App.css';
+
+interface TeamProps {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+const teamList = CollegeBasketballTeams.teams;
+
+function Heading() {
+  return <h1>WELCOME TO MARCH MADNESS!</h1>;
+}
+
+class Team extends React.Component<TeamProps> {
+  render() {
+    const oneTeam = this.props;
+    return (
+      <div>
+        <h3>Team: {oneTeam.school}</h3>
+        <h3>Mascot: {oneTeam.name}</h3>
+        <h3>
+          City/State: {oneTeam.city}, {oneTeam.state}
+        </h3>
+        <br />
+      </div>
+    );
+  }
+}
+
+function TeamList() {
+  return (
+    <div>
+      <h2>Team List</h2>
+      <br />
+
+      {teamList.map((oneTeam) => (
+        <Team {...oneTeam} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Heading />
+      <TeamList />
     </div>
   );
 }
